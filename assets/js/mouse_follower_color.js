3f80de9c33e392ae2c3c6e9257d782ca09ef7f50
Abhishek <abhishek.das0023@gmail.com>
2024-09-15 00:23:44 +0600

    mouse effect improved

---
+++ assets/js/mouse_follower_color.js
@@ -0,0 +1,7 @@
+footer.addEventListener('mouseenter', () => {
+    document.body.style.setProperty('--cursor-color', 'white');
+});
+
+footer.addEventListener('mouseleave', () => {
+    document.body.style.setProperty('--cursor-color', 'default');
+});
