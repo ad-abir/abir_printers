34e9dfd941997d27f2d722141d3ed6e486277b52
Abhishek <abhishek.das0023@gmail.com>
2024-11-27 00:43:10 +0600

    update: file removed

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo 'POST request received!';
-    echo 'Name: ' . $_POST['name'];
-    echo 'Email: ' . $_POST['email'];
-    echo 'Message: ' . $_POST['message'];
-} else {
-    echo 'Please use POST method.';
-}
